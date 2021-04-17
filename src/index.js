677a6fa404e50c5455a56c7ebb0e506265714384
Ray Crist <rayhcrist@gmail.com>
2021-04-16 22:39:33 -0400

    the end is insight

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import $ from 'jquery';
+import './style.scss';
+
+let num = 0;
 console.log('starting up!');
+
+setInterval(() => {
+  $('#main').html(`You've been on this page for ${num} seconds.`);
+  num += 1;
+},
+
+1000);
